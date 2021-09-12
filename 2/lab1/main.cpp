fa6a9414dd15ef6a28131d31923665c5627213a6
Alexander <dasewhuy@gmail.com>
2021-09-12 15:40:42 +0700

    variant 8

--- 2/lab1/main.cpp
+++ 2/lab1/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <ctime>
+#include <time.h>
 #include <string>
 #include <fstream>
 #include <sstream>
@@ -11,30 +11,16 @@ struct datetime
     int hour;
     int minute;
     int second;
-    
+
     int day;
     int month;
     int year;
 
+    void Output();
+    void System_time(datetime& system);
     void Get_next_date(datetime& next);
     void Get_prev_date(datetime& prev);
-    void System_time(datetime& system);
-
-    void Output();
 };
-
-void Get_normal_time(time_t unix_time, datetime& A);
-time_t Get_unix_time(datetime A);
-
-void datetime::Get_next_date(datetime& next)
-{
-    Get_normal_time(Get_unix_time(*this) + 86400, next);
-}
-
-void datetime::Get_prev_date(datetime& prev)
-{
-    Get_normal_time(Get_unix_time(*this) - 86400, prev);
-}
 
 void datetime::Output()
 {
@@ -43,43 +29,55 @@ void datetime::Output()
 
 void datetime::System_time(datetime& system)
 {
-    time_t now = time(0);
-    tm *ltm = localtime(&now);
-    system.hour = ltm->tm_hour;
-    system.minute = ltm->tm_min;
-    system.second = ltm->tm_sec;
-    
-    system.day = ltm->tm_mday;
-    system.month = ltm->tm_mon + 1;
-    system.year = ltm->tm_year + 1900;
+    time_t rawtime;
+    tm timeinfo;
+    time(&rawtime);
+    localtime_s(&timeinfo, &rawtime);
+    system.hour = timeinfo.tm_hour;
+    system.minute = timeinfo.tm_min;
+    system.second = timeinfo.tm_sec;
+    system.day = timeinfo.tm_mday;
+    system.month = timeinfo.tm_mon + 1;
+    system.year = timeinfo.tm_year + 1900;
 }
 
 time_t Get_unix_time(datetime A)
 {
-    time_t now = time(0);
-    tm *ltm;
-    time(&now);
-    ltm = localtime(&now);
-    ltm->tm_year = A.year - 1900;
-    ltm->tm_mon = A.month - 1;    
-    ltm->tm_mday = A.day;          
-    ltm->tm_hour = A.hour;         
-    ltm->tm_min = A.minute;          
-    ltm->tm_sec = A.second;  
-    time_t unix_time = mktime (ltm);
+    tm timeinfo;
+    timeinfo.tm_year = A.year - 1900;
+    timeinfo.tm_mon = A.month - 1;
+    timeinfo.tm_mday = A.day;
+    timeinfo.tm_hour = A.hour;
+    timeinfo.tm_min = A.minute;
+    timeinfo.tm_sec = A.second;
+    time_t unix_time = mktime(&timeinfo);
     return unix_time;
 }
 
 void Get_normal_time(time_t unix_time, datetime& A)
 {
-    tm *ltm = localtime(&unix_time);
-    A.hour = ltm->tm_hour;
-    A.minute = ltm->tm_min;
-    A.second = ltm->tm_sec;
-    
-    A.day = ltm->tm_mday;
-    A.month = ltm->tm_mon + 1;
-    A.year = ltm->tm_year + 1900;
+    tm timeinfo;
+    localtime_s(&timeinfo, &unix_time);
+    A.hour = timeinfo.tm_hour;
+    A.minute = timeinfo.tm_min;
+    A.second = timeinfo.tm_sec;
+    A.day = timeinfo.tm_mday;
+    A.month = timeinfo.tm_mon + 1;
+    A.year = timeinfo.tm_year + 1900;
+}
+
+void datetime::Get_next_date(datetime& next)
+{
+    int min = 2419200 * 2;
+    int max = 31536000;
+    Get_normal_time(Get_unix_time(*this) + (min + rand() % (max - min + 1)), next);
+}
+
+void datetime::Get_prev_date(datetime& prev)
+{
+    int min = 2419200 * 2;
+    int max = 31536000;
+    Get_normal_time(Get_unix_time(*this) - (min + rand() % (max - min + 1)), prev);
 }
 
 datetime* Read_file_date(string file_name, int& n)
@@ -87,34 +85,67 @@ datetime* Read_file_date(string file_name, int& n)
     ifstream file(file_name);
     string line;
     n = 0;
-    while(getline(file, line))
+    while (getline(file, line))
     {
         n++;
     }
     datetime* Date = new datetime[n];
     int i = 0;
-    file.close();
-    ifstream file2(file_name);
-    while(getline(file2, line))
+    file.clear();
+    file.seekg(0, ios::beg);
+    while (getline(file, line))
     {
         istringstream line_(line);
         line_ >> Date[i].hour >> Date[i].minute >> Date[i].second >> Date[i].day >> Date[i].month >> Date[i].year;
         i++;
     }
-    file2.close();
+    file.close();
     return Date;
 }
 
 int main()
 {
+    srand(time(0));
     datetime date;
     int n;
     datetime* Date = Read_file_date("data.txt", n);
-    for(int i = 0; i < n; i++)
+    datetime* Prev = new datetime[n];
+    date.System_time(date); printf("\n\nSystem time: "); date.Output();
+    printf("\n\n\tData read from file:\n\n");
+    for (int i = 0; i < n; i++)
     {
-        cout << i + 1 << ")\t"; Date[i].Output(); cout << "\t"; Date[i].Get_next_date(date); date.Output(); 
-        cout << "\t"; Date[i].Get_prev_date(date); date.Output(); cout << endl;
+        printf("%i)\t", i + 1);
+        Date[i].Output(); 
+        printf("\t");
+        Date[i].Get_next_date(date);
+        date.Output();
+        printf("\t");
+        Date[i].Get_prev_date(date);
+        date.Output();
+        printf("\n");
+        if (Date[i].year == date.year)
+        {
+            Prev[i] = date;
+        }
+    }
+    printf("\n\tVariant 8:\n\n");
+    int a = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (Prev[i].day > 0)
+        {
+            printf("%i)\t", i + 1);
+            Date[i].Output();
+            printf("\t");
+            Prev[i].Output();
+            printf("\n");
+        }
+        else
+        {
+            printf("\n");
+        }
     }
     delete[] Date;
+    delete[] Prev;
     return 0;
 }
