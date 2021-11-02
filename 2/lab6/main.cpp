2aab3a9772a18887a1d8b32f16ce5d6845225628
Antimagus <dasewhuy@gmail.com>
2021-11-02 17:30:54 +0700

    lab6

---
+++ 2/lab6/main.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include "Figure3D.h"
+
+using namespace std;
+
+int main()
+{
+	Cylinder* X = NULL;
+	int select;
+	cout << "Enter the height of the cylinder:\n";
+	int h;
+	cout << "h = "; cin >> h;
+	cout << "Select a figure: (1 - Circle, 2 - Triangle, 3 - Ring): "; cin >> select;
+	switch (select)
+	{
+	case 1:
+	{
+		double r;
+		cout << "Enter the radius:\n";
+		cout << "r = "; cin >> r;
+		X = CircleCylinder::CreateInstance(r, h);
+		break;
+	}
+	case 2:
+	{
+		double a, b, c;
+		cout << "Enter the sides of the triangle:\n";
+		cout << "a = "; cin >> a;
+		cout << "b = "; cin >> b;
+		cout << "c = "; cin >> c;
+		X = TriangleCylinder::CreateInstance(a, b, c, h);
+		break;
+	}
+	case 3:
+	{
+		double r1, r2;
+		cout << "Enter the ring radii:\n";
+		cout << "r1 = "; cin >> r1;
+		cout << "r2 = "; cin >> r2;
+		X = RingCylinder::CreateInstance(r1, r2, h);
+		break;
+	}
+	default:
+		return 0;
+		break;
+	}
+	cout << "The resulting volume of the figure:\n";
+	cout << X->CalcVolume();
+}
