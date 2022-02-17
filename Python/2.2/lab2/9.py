cb687bda5aadc8f92bb8c5b490ced66e7538e156
lunfox <dasewhuy@gmail.com>
2022-02-17 19:43:41 +0700

    lab2 task9 fix

--- Python/2.2/lab2/9.py
+++ Python/2.2/lab2/9.py
@@ -9,7 +9,7 @@ for i in range(0, n):
     y.append(y_input)
 
 for i in range(n):
-    for j in range(n):
+    for j in range(i + 1, n):
         if x[i] == x[j] or y[i] == y[j] or abs(x[i] - x[j]) == abs(y[i] - y[j]):
             correct = False
 
