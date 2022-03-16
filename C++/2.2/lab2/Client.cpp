3f13f4682c0d5aae04292c325fcaad3a9e0b633e
lunfox <dasewhuy@gmail.com>
2022-03-16 17:22:05 +0700

    ClassFactory

---
+++ C++/2.2/lab2/Client.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "Iface.h"
+
+using namespace std;
+int main()
+{
+    IServerFactory* serverFactory;
+    IServer2* iServer;
+    H_RESULT res = GetClassObject(CLSIDServer, IID_IClassFactory, (void**)&serverFactory);
+    if(res != S_OK) return res;
+    res = serverFactory->CreateInstance(IID_IServer2, (void**)&iServer);
+    if(res != S_OK) return res;
+    iServer->Func2();
+}
