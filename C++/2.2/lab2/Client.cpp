e9934c601ac65e7a598fd50e7c949b093c3d8a4b
lunfox <dasewhuy@gmail.com>
2022-04-05 19:30:10 +0700

    Server update

--- C++/2.2/lab2/Client.cpp
+++ C++/2.2/lab2/Client.cpp
@@ -1,20 +1,55 @@
 #include <iostream>
 #include "Iface.h"
 
-using namespace std;
+class Server
+{
+    private:
+        I_ClassFactory* factory;
+        IServer* iServer;
+        IServer2* iServer2;
+
+    public:
+        void Func();
+        void Func2();
+        Server();
+        ~Server();
+        Server& operator=(Server& newServer);
+};
+
+Server::Server()
+{
+    GetClassObject(CLSIDServer, IID_IClassFactory, (void**)&factory);
+    factory->CreateInstance(IID_IServer, (void**)&iServer);
+    factory->Release();
+    iServer->QueryInterface(IID_IServer2, (void**)&iServer2);
+}
+
+Server::~Server() 
+{
+    iServer->Release();
+    iServer2->Release();
+}
+
+void Server::Func()
+{
+    iServer->Func();
+}
+
+void Server::Func2()
+{
+    iServer2->Func2();
+}
+
+Server& Server::operator=(Server& newServer) 
+{
+    return newServer;
+}
+
 int main()
 {
-    I_ClassFactory* serverFactory;
-    IServer2* iServer2;
-    H_RESULT res = GetClassObject(CLSIDServer, IID_IClassFactory, (void**)&serverFactory);
-    if(res != S_OK) return res;
-    res = serverFactory->CreateInstance(IID_IServer2, (void**)&iServer2);
-    serverFactory->Release();
-    if(res != S_OK) return res;
-    iServer2->Func2();
-    IServer* iServer;
-    iServer2->QueryInterface(IID_IServer, (void**)&iServer);
-    iServer->Func();
-    iServer2->Release();
-    iServer->Release();
+    Server server;
+    Server server2;
+    server = server2;
+    server.Func();
+    server.Func2();
 }
