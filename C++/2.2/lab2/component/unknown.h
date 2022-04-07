6aed6fd92682694110c5f8ee71e1588f47380b8f
lunfox <dasewhuy@gmail.com>
2022-04-07 17:48:22 +0700

    lab2

--- C++/2.2/lab2/component/unknown.h
+++ C++/2.2/lab2/component/unknown.h
@@ -3,15 +3,10 @@ typedef int I_ID;
 typedef int CLS_ID;
 typedef int U_LONG;
 
-const I_ID IID_IUnknown = 0;
-const I_ID IID_IServer = 1;
-const I_ID IID_IServer2 = 2;
-const I_ID IID_IClassFactory = 10;
 const H_RESULT S_OK = 1111;
 const H_RESULT E_NOINTERFACE = -1;
 const H_RESULT E_OUTOFMEMORY = -2;
 const H_RESULT E_CLASSNOTAVAILABLE = -3;
-const CLS_ID CLSIDServer = 101;
 
 class I_Unknown {
     public:
@@ -29,19 +24,3 @@ class I_ClassFactory : public I_Unknown
         U_LONG AddRef();
         U_LONG Release();
 };
-
-U_LONG I_ClassFactory::AddRef() 
-{
-    countReference++;
-    return countReference;
-}
-
-U_LONG I_ClassFactory::Release() 
-{
-    countReference--;
-    if(countReference == 0)
-    {
-        delete this;
-    }
-    return countReference;
-}
